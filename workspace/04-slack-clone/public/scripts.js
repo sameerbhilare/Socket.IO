060bbc413ffe254f9d07e1a1fa7fe883fcd2d126
Sameer Bhilare <bhilare.sameer@gmail.com>
2020-12-28 10:15:45 +0530

    Completed steps  1 - 3
    1. JOIN THE MAIN NAMESPACE ('/')
    2. AS SOON AS SOMEBODY CONNECTS TO MAIN NS, SEND BACK NAMESPACES INFO
    3. UPDATE DOM WITH NAMESPACE DATA

--- workspace/04-slack-clone/public/scripts.js
+++ workspace/04-slack-clone/public/scripts.js
@@ -1,5 +1,29 @@
 const socket = io('http://localhost:3000'); // this is a '/' namespace/endpoint
-const adminSocket = io('http://localhost:3000/admin'); // this is a '/admin' namespace/endpoint
+
+socket.on('connect', () => {
+  console.log(socket.id + ' connected.');
+});
+
+// listen for 'nsList' event, which will have all namespaces
+socket.on('nsList', (nsData) => {
+  /* 3. UPDATE DOM WITH NAMESPACE DATA */
+  let namespacesDiv = document.querySelector('.namespaces');
+  namespacesDiv.innerHTML = '';
+  nsData.forEach((ns) => {
+    // 'ns' is a random attribute here
+    namespacesDiv.innerHTML += `<div class="namespace" ns="${ns.endpoint}"><img src="${ns.img}"></div>`;
+  });
+
+  // Add click listeners for each namespace
+  Array.from(document.getElementsByClassName('namespace')).forEach(
+    (nsElement) => {
+      nsElement.addEventListener('click', (event) => {
+        const endpoint = nsElement.getAttribute('ns'); // get endpoint name attached to 'ns' attribute of 'namespace' div
+        console.log(`${endpoint} I should go to now.`);
+      });
+    }
+  );
+});
 
 socket.on('messageFromServer', (dataFromServer) => {
   console.log(dataFromServer);
@@ -10,11 +34,6 @@ socket.on('messageFromServer', (dataFromServer) => {
 // the client has no idea that it's part of a room. It's just listening to the name space for event 'joined'
 socket.on('joined', (msg) => {
   console.log(msg);
-});
-
-adminSocket.on('welcome', (dataFromServer) => {
-  // you will see the SAME socket.id for both at server side and here at client side.
-  console.log(dataFromServer);
 });
 
 document.getElementById('user-input').addEventListener('submit', (event) => {
--- workspace/04-slack-clone/slack.js
+++ workspace/04-slack-clone/slack.js
@@ -4,8 +4,6 @@ const namespaces = require('./data/namespaces');
 
 const app = express();
 
-console.log(namespaces);
-
 // serving static files
 app.use(express.static(__dirname + '/public'));
 
@@ -17,41 +15,26 @@ const expressServer = app.listen(3000);
 // 'io' is our socketio server
 const io = socketio(expressServer);
 
+/* 1. JOIN THE MAIN NAMESPACE ('/') */
 // when someone connects to socketio server, this callback runs
 // 'connection' and 'connect' built-in events are literally exactly the same.
-// io.on() is SAME as io.of('/).on()
+// io.on() is SAME as io.of('/).on() => main namespace
 io.on('connection', (socket) => {
-  // you will see the SAME socket.id for both at client side and here at server side.
-  console.log(`socket.id => ${socket.id}`);
+  /* 2. AS SOON AS SOMEBODY CONNECTS TO MAIN NS, SEND BACK NAMESPACES INFO */
 
-  // send message to client by emitting custom event 'messageFromServer'
-  socket.emit('messageFromServer', {
-    data: 'Welcome to the socket.io server!',
+  // A) build an array to send back img and endpoint for each namespace.
+  let nsData = namespaces.map((ns) => {
+    return { img: ns.img, endpoint: ns.endpoint };
   });
 
-  // listen to custom event 'messageToServer' from client
-  socket.on('messageToServer', (dataFromClient) => {
-    console.log(dataFromClient);
-  });
-
-  // join a room
-  socket.join('level1');
-  // only this client will get this msg
-  socket.emit(
-    'joined',
-    `${socket.id} has joined the level1 room. (Sending from socket)`
-  );
-  // all sockets connected to this namespace will get this message.Since it is sent from namespace.
-  // io.of('/') returns main namespace
-  io.of('/').emit(
-    'joined',
-    `${socket.id} has joined the level1 room. (Sending from Namespace)`
-  );
+  // B) Send the nsData back to client.
+  // Need to use socket, NOT io bcz we want to the data only to this client, NOT to everybody
+  socket.emit('nsList', nsData);
 });
 
-io.of('/admin').on('connection', (socket) => {
-  console.log(
-    'Someone just connected to admin namespace. Admin socket.id=' + socket.id
-  );
-  io.of('/admin').emit('welcome', 'Welcome to the admin channel/namespace.');
+// loop through each namespace and listen for connection
+namespaces.forEach((namespace) => {
+  io.of(namespace.endpoint).on('connection', (socket) => {
+    console.log(`${socket.id} has joined namespace ${namespace.endpoint}`);
+  });
 });
